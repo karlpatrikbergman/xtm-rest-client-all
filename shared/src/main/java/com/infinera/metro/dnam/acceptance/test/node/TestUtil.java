274fe2da3e47576ba5a9f31493ab544ca82cdcac
Patrik Bergman <patrik.bergman@infinera.com>
2017-04-25 15:44:51 +0200

    Added factory for Node

---
+++ shared/src/main/java/com/infinera/metro/dnam/acceptance/test/node/TestUtil.java
@@ -0,0 +1,5 @@
+package com.infinera.metro.dnam.acceptance.test.node;
+
+//TODO: Move test verification here?
+public class TestUtil {
+}
---
+++ xtm-rest-client-factory/src/test/java/com/infinera/metro/dnam/acceptance/test/factory/NodeFactoryTest.java
@@ -0,0 +1,100 @@
+package com.infinera.metro.dnam.acceptance.test.factory;
+
+import com.infinera.metro.dnam.acceptance.test.mib.Board;
+import com.infinera.metro.dnam.acceptance.test.mib.BoardEntry;
+import com.infinera.metro.dnam.acceptance.test.mib.Operation;
+import com.infinera.metro.dnam.acceptance.test.node.Node;
+import com.infinera.metro.dnam.acceptance.test.node.NodeAccessData;
+import com.infinera.metro.dnam.acceptance.test.node.dto.AnswerObject;
+import com.infinera.metro.dnam.acceptance.test.node.dto.AnswerObjects;
+import com.infinera.metro.dnam.acceptance.test.node.dto.AttributeObject;
+import com.palantir.docker.compose.DockerComposeRule;
+import com.palantir.docker.compose.connection.waiting.HealthChecks;
+import org.junit.ClassRule;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.util.Optional;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class NodeFactoryTest {
+    @ClassRule
+    public static DockerComposeRule docker = DockerComposeRule.builder()
+            .file("src/test/resources/docker-compose.yml")
+            .waitingForService("node1", HealthChecks.toHaveAllPortsOpen())
+            .build();
+
+    private Node node;
+
+    private final BoardEntry BOARD_ENTRY = BoardEntry.builder()
+            .board(Board.TP10G)
+            .subrack(1)
+            .slot(2)
+            .build();
+
+    private final NodeAccessData nodeAccessData = NodeAccessData.builder()
+            .ipAddress("172.45.0.101")
+            .port(80)
+            .userName("root")
+            .password("root")
+            .build();
+
+    @Test
+    public void foo() throws IOException {
+        node = NodeFactory.INSTANCE.createNode(nodeAccessData);
+
+        //Given
+        boardIsNotAdded(BOARD_ENTRY);
+
+        //When
+        AnswerObjects answerObjects = node.createBoard(BOARD_ENTRY);
+
+        //Then
+        createBoardSucceeded(answerObjects);
+
+        getBoardSuccessful(BOARD_ENTRY);
+
+        //Clean up after test
+        node.deleteBoard(BOARD_ENTRY);
+
+
+    }
+
+    private void getBoardSuccessful(BoardEntry boardEntry) throws IOException {
+        AnswerObjects answerObjects = node.getBoard(boardEntry);
+        Optional<AnswerObject> answerObjectOptional = answerObjects.getAnswerObject(Operation.get.getName(), boardEntry.getMibString());
+        assertThat("AnswerObject.operation=get and entry="+BOARD_ENTRY.getMibString()+" exists", answerObjectOptional.isPresent(), is(true));
+
+        AnswerObject answerObject = answerObjectOptional.get();
+        assertThat("AnswerObject.isSuccess=true" , answerObject.isSuccess(), is(true));
+
+        Optional<AttributeObject> attributeObjectOptional = answerObject.getAttributeObject("equipmentBoardName");
+        assertThat("AttributeObject.name=equipmentBoardName' exists", attributeObjectOptional.isPresent(), is(true));
+        AttributeObject attributeObject = attributeObjectOptional.get();
+        assertThat("AttributeObject.value="+BOARD_ENTRY.getMibString(), attributeObject.getValue(), is(BOARD_ENTRY.getMibString()));
+    }
+
+    private void createBoardSucceeded(AnswerObjects answerObjects) {
+        Optional<AnswerObject> answerObjectOptional = answerObjects.getAnswerObject(Operation.create.getName()); //Create response has no entry set in R-attributes
+        assertThat("AnswerObject.operation=create' and 'entry="+BOARD_ENTRY.getMibString()+"' exists", answerObjectOptional.isPresent(), is(true));
+
+        AnswerObject answerObject = answerObjectOptional.get();
+        assertThat("AttributeObject.isSuccess=true" , answerObject.isSuccess(), is(true));
+
+        Optional<AttributeObject> attributeObjectOptional = answerObject.getAttributeObject(BOARD_ENTRY.getMibString());
+        assertThat("AttributeObject.name=" + BOARD_ENTRY.getMibString(), attributeObjectOptional.isPresent(), is(true));
+    }
+
+    private void boardIsNotAdded(BoardEntry boardEntry) throws IOException {
+        AnswerObjects answerObjects = node.getBoard(boardEntry);
+        Optional<AnswerObject> answerObjectOptional = answerObjects.getAnswerObject(Operation.error.getName());
+        assertThat("AnswerObject.operation=error exists", answerObjectOptional.isPresent(), is(true));
+
+        AnswerObject answerObject = answerObjectOptional.get();
+        assertThat("AnswerObject.entry=" + BOARD_ENTRY.getMibString(), answerObject.getEntry(), is(BOARD_ENTRY.getMibString()));
+        assertThat("AnswerObject.isSuccess=false" , answerObject.isSuccess(), is(false));
+        assertThat("Error message contains 'Entry not found'", answerObject.getError().contains("Entry not found"), is(true));
+    }
+}

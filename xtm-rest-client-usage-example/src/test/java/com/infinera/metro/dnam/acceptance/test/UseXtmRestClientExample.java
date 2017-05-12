56574c2ca2df4d94750b93c5b8cc075e0739d464
Patrik Bergman <patrik.bergman@infinera.com>
2017-05-12 13:16:11 +0200

    Minor changes

--- xtm-rest-client-usage-example/src/test/java/com/infinera/metro/dnam/acceptance/test/UseXtmRestClientExample.java
+++ xtm-rest-client-usage-example/src/test/java/com/infinera/metro/dnam/acceptance/test/UseXtmRestClientExample.java
@@ -1,20 +1,30 @@
 package com.infinera.metro.dnam.acceptance.test;
 
-import com.infinera.metro.dnam.acceptance.test.node.*;
+import com.infinera.metro.dnam.acceptance.test.node.IntegrationTest;
+import com.infinera.metro.dnam.acceptance.test.node.NodeAccessData;
+import com.infinera.metro.dnam.acceptance.test.node.NodeImpl;
 import com.infinera.metro.dnam.acceptance.test.node.configuration.NodeConfiguration;
 import com.infinera.metro.dnam.acceptance.test.node.configuration.NodeEquipment;
 import com.infinera.metro.dnam.acceptance.test.node.configuration.ObjectFromFileUtil;
 import com.infinera.metro.dnam.acceptance.test.node.configuration.PeerConfiguration;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
+import org.junit.experimental.categories.Category;
 
 import java.io.IOException;
 
+@Category(IntegrationTest.class)
 @Slf4j
 public class UseXtmRestClientExample {
 
     private final NodeAccessData nodeAccessDataNodeA, nodeAccessDataNodeZ;
     private final NodeEquipment nodeEquipmentNodeA, nodeEquipmentNodeZ;
+
+//    @ClassRule
+//    public static DockerComposeRule docker = DockerComposeRule.builder()
+//            .file("src/test/resources/docker-compose.yml")
+//            .waitingForService("node1", HealthChecks.toHaveAllPortsOpen())
+//            .build();
 
     public UseXtmRestClientExample() throws IOException {
         nodeAccessDataNodeA = ObjectFromFileUtil.INSTANCE.getObject("test-case-x-node-config/node_a_access_data.yaml", NodeAccessData.class);

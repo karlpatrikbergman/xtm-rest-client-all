92dc907b004bb25104b7997213ea564ff3bc4f2c
Patrik Bergman <patrik.bergman@infinera.com>
2017-06-22 08:36:32 +0200

    Change variable names for better readability

--- xtm-rest-client-usage-example/src/test/java/com/infinera/metro/dnam/acceptance/test/dockercompose/kinell/KinellEquipmentSerializationTest.java
+++ xtm-rest-client-usage-example/src/test/java/com/infinera/metro/dnam/acceptance/test/dockercompose/kinell/KinellEquipmentSerializationTest.java
@@ -104,144 +104,35 @@ public class KinellEquipmentSerializationTest {
                 )
             )
             .build();
-//
-//        Tphex10gotn tphex10gotn = Tphex10gotn.builder()
-//            .subrack(1)
-//            .slot(Slot.slot6)
-//            .build();
-//
-//        Tp100gotn tp100gotn = Tp100gotn.builder()
-//            .subrack(1)
-//            .slot(Slot.slot7)
-//            .build();
-//
-//        Gbe10Emxp10Gii gbe10Emxp10Gii = Gbe10Emxp10Gii.builder()
-//            .subrack(1)
-//            .slot(Slot.slot14)
-//            .build();
-//
+
         Mdu40EvenL mdu40EvenL = Mdu40EvenL.builder()
             .subrack(1)
             .slot(Slot.slot15)
             .build();
-
-        //Here I get error: "Can't create board, Board to the left has multi-slot with"
-//        Oiuc50100 oiuc50100 = Oiuc50100.builder()
-//            .subrack(1)
-//            .slot(Slot.slot18)
-//            .build();
 
         final Map<Slot, Board> boards = new LinkedHashMap<>();
         boards.put(roadm1x2G50_1.getSlot(), roadm1x2G50_1);
         boards.put(oa2x17.getSlot(), oa2x17);
         boards.put(roadm1x2G50_2.getSlot(), roadm1x2G50_2);
         boards.put(ocm2p.getSlot(), ocm2p);
-//        boards.put(tphex10gotn.getSlot(), tphex10gotn);
-//        boards.put(tp100gotn.getSlot(), tp100gotn);
-//        boards.put(gbe10Emxp10Gii.getSlot(), gbe10Emxp10Gii);
-//        boards.put(mdu40EvenL.getSlot(), mdu40EvenL);
-//        boards.put(oiuc50100.getSlot(), oiuc50100);
+        boards.put(mdu40EvenL.getSlot(), mdu40EvenL);
 
         final NodeEquipment nodeEquipment = NodeEquipment.builder()
             .boards(boards)
             .build();
+
+        //To apply directly
+        //nodeEquipment.applyTo(node);
+
+        //Get yaml string from node equipment object
         final ObjectMapper mapper = ObjectFromFileUtilJackson.INSTANCE.getMapper();
         final String yamlString = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(nodeEquipment);
-
         log.info(yamlString);
 
+        //Create yaml configuration file out of yaml string
         //TODO: Create file util class?
         Path newFilePath = Paths.get("src/test/resources/dockercompose/kinell/node_liljeholmen.yml");
         Files.write(newFilePath, yamlString.getBytes());
 
     }
 }
-
-/*
-        Tpd10gbe tpd10gbe = Tpd10gbe.builder()
-            .subrack(1)
-            .slot(Slot.slot2)
-            .clientPorts(
-                Arrays.asList(
-                    Port.builder()
-                        .transmitPort(1)
-                        .receivePort(2)
-                        .portAttributes(
-                            Arrays.asList(
-                                ClientPortConfigAttributes.of(
-                                    ConfigurationList.of(Configuration.builder()
-                                        .key("clientIfConfigurationCommand")
-                                        .value("wan10GbE yes")
-                                        .build()
-                                    )
-                                ),
-                                ClientPortSetAttributes.of(
-                                    ConfigurationList.of(Configuration.builder()
-                                        .key("expectedFrequency")
-                                        .value("w1530")
-                                        .build()
-                                    )
-                                )
-                            )
-                        )
-                        .build(),
-                    Port.builder()
-                        .transmitPort(5)
-                        .receivePort(6)
-                        .portAttributes(
-                            Arrays.asList(
-                                ClientPortConfigAttributes.of(
-                                    ConfigurationList.of(Configuration.builder()
-                                        .key("clientIfConfigurationCommand")
-                                        .value("wan10GbE yes")
-                                        .build()
-                                    )
-                                ),
-                                ClientPortSetAttributes.of(
-                                    ConfigurationList.of(Configuration.builder()
-                                        .key("expectedFrequency")
-                                        .value("w1530")
-                                        .build()
-                                    )
-                                )
-                            )
-                        )
-                        .build()
-                )
-            )
-            .linePorts(
-                Arrays.asList(
-                    Port.builder()
-                        .transmitPort(3)
-                        .receivePort(4)
-                        .portAttributes(
-                            Arrays.asList(
-                                LinePortSetAttributes.of(ConfigurationList.of(
-                                    Configuration.builder()
-                                        .key("expectedFrequency")
-                                        .value("ch926")
-                                        .build()
-                                    )
-                                )
-                            )
-                        )
-                        .build(),
-                    Port.builder()
-                        .transmitPort(7)
-                        .receivePort(8)
-                        .portAttributes(
-                            Arrays.asList(
-                                LinePortSetAttributes.of(ConfigurationList.of(
-                                    Configuration.builder()
-                                        .key("expectedFrequency")
-                                        .value("ch927")
-                                        .build()
-                                    )
-                                )
-                            )
-                        )
-                        .build()
-                )
-            )
-            .build();
-        */
--- xtm-rest-client-usage-example/src/test/java/com/infinera/metro/dnam/acceptance/test/dockercompose/kinell/KinellSetupTest.java
+++ xtm-rest-client-usage-example/src/test/java/com/infinera/metro/dnam/acceptance/test/dockercompose/kinell/KinellSetupTest.java
@@ -3,8 +3,10 @@ package com.infinera.metro.dnam.acceptance.test.dockercompose.kinell;
 import com.infinera.metro.dnam.acceptance.test.node.DontLetGradleRun;
 import com.infinera.metro.dnam.acceptance.test.node.NodeAccessData;
 import com.infinera.metro.dnam.acceptance.test.node.NodeImpl;
-import com.infinera.metro.dnam.acceptance.test.node.configuration.*;
-import com.infinera.metro.dnam.acceptance.test.node.configuration.board.Board;
+import com.infinera.metro.dnam.acceptance.test.node.configuration.NodeConfiguration;
+import com.infinera.metro.dnam.acceptance.test.node.configuration.NodeEquipment;
+import com.infinera.metro.dnam.acceptance.test.node.configuration.ObjectFromFileUtil;
+import com.infinera.metro.dnam.acceptance.test.node.configuration.ObjectFromFileUtilJackson;
 import com.infinera.metro.dnam.acceptance.test.node.dockercompose.DockerCompose;
 import com.infinera.metro.dnam.acceptance.test.node.dockercompose.Service;
 import lombok.extern.slf4j.Slf4j;
@@ -18,8 +20,8 @@ import static org.junit.Assert.assertNotNull;
 public class KinellSetupTest {
 
     final ObjectFromFileUtil objectFromFileUtil = ObjectFromFileUtilJackson.INSTANCE;
-    private final NodeEquipment nodeEquipmentNodeA;
-    private final NodeAccessData nodeAccessDataNodeA;
+    private final NodeEquipment liljeholmenNodeEquipment;
+    private final NodeAccessData liljeholmenNodeAccessData;
     private final String RESOURCES_PATH = "dockercompose/kinell/";
     private final String NODE_EQUIPMENT_PATH = RESOURCES_PATH + "node_liljeholmen.yml";
     private final String DOCKER_COMPOSE_PATH = RESOURCES_PATH + "docker-compose.yml";
@@ -27,28 +29,22 @@ public class KinellSetupTest {
 
     public KinellSetupTest() {
         final DockerCompose dockerCompose = objectFromFileUtil.getObject(DOCKER_COMPOSE_PATH, DockerCompose.class);
-        final Service serviceNodeA = dockerCompose.getServices().get("liljeholmen");
-        final String dockerComposeIpAddressNodeA = serviceNodeA.getNetworks().get("sthlm_network").get("ipv4_address");
+        final Service liljeholmenNode = dockerCompose.getServices().get("liljeholmen");
+        final String liljeholmenIpAddress = liljeholmenNode.getNetworks().get("sthlm_network").get("ipv4_address");
 
-        nodeEquipmentNodeA = objectFromFileUtil.getObject(NODE_EQUIPMENT_PATH, NodeEquipment.class);
-        assertNotNull(nodeEquipmentNodeA);
+        liljeholmenNodeEquipment = objectFromFileUtil.getObject(NODE_EQUIPMENT_PATH, NodeEquipment.class);
+        assertNotNull(liljeholmenNodeEquipment);
 
-        Board boardSlot2 = nodeEquipmentNodeA.getBoard(Slot.slot2);
-        assertNotNull(boardSlot2);
-
-        nodeAccessDataNodeA = objectFromFileUtil.getObject(NODE_ACCESS_DATA_PATH, NodeAccessData.class)
-                .copyObjectAndChangeIpAddress(dockerComposeIpAddressNodeA);
-
+        liljeholmenNodeAccessData = objectFromFileUtil.getObject(NODE_ACCESS_DATA_PATH, NodeAccessData.class)
+                .copyObjectAndChangeIpAddress(liljeholmenIpAddress);
     }
 
     @Test
     public void test() {
         final NodeConfiguration nodeConfigurationNodeA = NodeConfiguration.builder()
-                .node(NodeImpl.create(nodeAccessDataNodeA))
-                .nodeEquipment(nodeEquipmentNodeA)
+                .node(NodeImpl.create(liljeholmenNodeAccessData))
+                .nodeEquipment(liljeholmenNodeEquipment)
                 .build();
         nodeConfigurationNodeA.apply();
-
     }
-
 }

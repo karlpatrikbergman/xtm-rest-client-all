51c1a9c007c9864384eaeacc25b8e123a47c4a82
Patrik Bergman <patrik.bergman@infinera.com>
2017-11-10 15:24:38 +0100

    Changed docker_clean_up.sh

--- scripts/docker_clean_up.sh
+++ scripts/docker_clean_up.sh
@@ -3,4 +3,4 @@
  docker rm -v $(docker ps -a -q -f status=exited)
  docker rmi $(docker images -f "dangling=true" -q)
  docker volume rm $(docker volume ls -qf dangling=true)
- docker network rm -f nodeimpl_smoke_test_network node_connect_test_network
+ docker network rm nodeimpl_smoke_test_network node_connect_test_network
